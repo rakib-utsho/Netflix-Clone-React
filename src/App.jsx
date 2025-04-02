050d25c2568aa5134612b90326c5d03b5ff181f7
rakib-utsho <rakibutsho1920@gmail.com>
2025-04-02 21:21:08 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/Home/Home.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_icon from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* logo and menu */}
+      <div className="navbar-left">
+        <img src={logo} alt="main logo" />
+        <ul>
+          <li>Home</li>
+          <li>TV Show</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Language</li>
+        </ul>
+      </div>
+      {/* search & navbar profile */}
+      <div className="navbar-right">
+        <img src={search} alt="search-icon" className="icons" />
+        <p>Children</p>
+        <img src={bell_icon} alt="bell-icon" className="icons" />
+
+        {/* navbar profile */}
+        <div className="navbar-profile">
+          <img src={profile_icon} alt="profile-image" className="profile" />
+          <img src={caret_icon} alt="profile-image" className="profile" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
