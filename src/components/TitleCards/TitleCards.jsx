002ebb08f4a54821b1a1a285cff507004976964e
rakib-utsho <rakibutsho1920@gmail.com>
2025-04-02 23:54:17 +0600

    update component

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import './TitleCards.css'
+import React, { useEffect, useRef } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data.js";
+
 
 const TitleCards = () => {
-  return (
-    <div className='titleCards'>TitleCards</div>
-  )
+
+  const cardsRef = useRef();
+
+const handleWheel = (event) => {
+  event.preventDefault() += event.deltaY;
 }
 
-export default TitleCards
+useEffect(() => {
+  cardsRef.current.addEventListener("wheel", handleWheel);
+}, []);
+
+  return (
+    <div className="titleCards">
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="movie-image" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TitleCards;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,9 @@ import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import hero from "../../assets/hero_banner.jpg";
 import hero2 from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
+import TitleCards from "../../components/TitleCards/TitleCards.jsx";
 
 const Home = () => {
   return (
@@ -13,7 +16,14 @@ const Home = () => {
           <img src={hero} alt="hero-banner" className="banner-img"/>
           {/* sub hero menu */}
           <div className="hero-caption">
-            <img src={hero2} alt="hero-title" />
+            <img src={hero2} alt="hero-title" className="caption-img"/>
+            <p>Discovering his ties to a secret ancient order, a young man living in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+            <div className="hero-btns">
+              <button className="btn"><img src={play_icon} alt="play-icon" />Play</button>
+              <button className="btn dark-btn"><img src={info_icon} alt="" />More Info</button>
+            </div>
+            {/* movie title cards */}
+            <TitleCards/>
           </div>
       </div>
     </div>
