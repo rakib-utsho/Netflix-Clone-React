e1ae68ffaee1f5b70d51d9a6b83fa8cbcf2ad47d
rakib-utsho <rakibutsho1920@gmail.com>
2025-04-03 23:17:31 +0600

    fetch data from api and and add using functionality

--- src/pages/Player/Player.jsx
+++ src/pages/Player/Player.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Player.css";
 import back_arrow_icon from "../../assets/back_arrow_icon.png";
+import { useParams } from "react-router-dom";
 
 const Player = () => {
+  const { id } = useParams();
+  const [apiData, setApiData] = useState({
+    name: "",
+    key: "",
+    published_at: "",
+    type: "",
+  });
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTFiOGE2Zjk1OWQ0ZTZkNWI3ZTliZmYzODVmZjQ3YyIsIm5iZiI6MTc0MzY4OTczNS40MzM5OTk4LCJzdWIiOiI2N2VlOTgwNzAzNTQwY2Y4ZTU2MmJmZDgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.MHTyh8Olktz9g_E8pwKNFGrFhPKt1LwjS86tSaaYLKE",
+    },
+  };
+
+  useEffect(() => {
+    // Fetch data from specific id TMDB API
+    fetch(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results[0]))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="player">
       <img src={back_arrow_icon} alt="" />
       <iframe
-        src="https://www.youtube.com/embed/sA0WGdeexOU"
+        src={`https://www.youtube.com/embed/${apiData.key}`}
         frameborder="0"
         width="90%"
         height="90%"
@@ -15,9 +44,9 @@ const Player = () => {
         allowFullScreen
       ></iframe>
       <div className="player-info">
-        <p>Published Date</p>
-        <p>Name</p>
-        <p>Type</p>
+        <p>{apiData.published_at.slice(0, 10)}</p>
+        <p>{apiData.name}</p>
+        <p>{apiData.type}</p>
       </div>
     </div>
   );
