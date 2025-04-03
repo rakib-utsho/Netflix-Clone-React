ae75ca8946a9f5f160626d4bc3d060c86f29a06f
rakib-utsho <rakibutsho1920@gmail.com>
2025-04-03 21:01:12 +0600

    fetch data from TMDB

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -2,31 +2,37 @@ import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
 import cards_data from "../../assets/cards/Cards_data.js";
 
-
 const TitleCards = () => {
-
   const cardsRef = useRef();
 
-const handleWheel = (event) => {
-  event.preventDefault() += event.deltaY;
-}
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY; // Horizontal scrolling
+  };
 
-useEffect(() => {
-  cardsRef.current.addEventListener("wheel", handleWheel);
-}, []);
+  useEffect(() => {
+    const cardsElement = cardsRef.current;
+    if (cardsElement) {
+      cardsElement.addEventListener("wheel", handleWheel);
+    }
+
+    return () => {
+      if (cardsElement) {
+        cardsElement.removeEventListener("wheel", handleWheel);
+      }
+    };
+  }, []);
 
   return (
     <div className="titleCards">
       <h2>Popular on Netflix</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => {
-          return (
-            <div className="card" key={index}>
-              <img src={card.image} alt="movie-image" />
-              <p>{card.name}</p>
-            </div>
-          );
-        })}
+        {cards_data.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.image} alt="movie" />
+            <p>{card.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
